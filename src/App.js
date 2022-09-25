9b2e132155af4f2498180b08b06614f360888be8
Syed Misbah Ur Rehman <syedmisbah588@gmail.com>
2022-09-26 02:09:12 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
 
 function App() {
+
+  const [name,setName]=useState("");
+  const [tnc,setTnc]=useState(false);
+  const [gender,setGender]=useState('');
+  const [data,setData]=useState('');
+  function getFormData(e)
+  {
+    console.warn(name,tnc,gender,data)
+    e.preventDefault()
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Handle Form in React</h1>
+     <form onSubmit={getFormData}>
+     <label htmlFor="fname"><b>Fullname </b></label>
+       <input type="text" placeholder="enter name" value={name} onChange={(e)=>setName(e.target.value)} name="fname"/> <br /><br />
+       
+       <label htmlFor="gender"><b>Gender </b></label>
+        <select name ="gender" onChange={(e)=>setGender(e.target.value)}>
+         <option>Male</option>
+         <option>Female</option>
+         <option>Other</option>
+
+       </select> <br /><br />
+       <input type="checkbox"  onChange={(e)=>setTnc(e.target.checked)} /><span>Agree to Terms and Conditions</span>
+       <br /><br />
+
+     
+     <div className="uploadimage">
+                <label htmlFor="imgs"><b>Upload Files</b></label>
+            </div>
+            <input id="imgs" type="file" accept="image/png, image/jpeg,.txt,.doc,.pdf" onChange={(e)=>setData(e.target.files)} />
+            <button type="submit">Submit</button>
+            </form>
     </div>
+    
   );
 }
+  
 
 export default App;
